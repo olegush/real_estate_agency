1de0b13978e91a5ff825e1fc1982933638c7fad1
olegush <oleg107117@gmail.com>
2019-08-14 10:27:21 +0300

    Add liked_by field

--- property/models.py
+++ property/models.py
@@ -24,6 +24,8 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, db_index=True)
     new_building = models.NullBooleanField("Новостройка", null=True)
 
+    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", related_name="liked_flats")
+
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
 
