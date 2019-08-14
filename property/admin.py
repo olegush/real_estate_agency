0a2d6c739a1f9c8f988d35eb31d0391c638a72c8
olegush <oleg107117@gmail.com>
2019-08-14 09:36:47 +0300

    Add complaint model

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
+
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
@@ -9,3 +10,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('user', 'flat', 'text')
+    raw_id_fields = ('user', 'flat')
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -25,3 +26,8 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User, verbose_name="Кто пожаловался", null=True, on_delete=models.SET_NULL)
+    flat = models.ForeignKey(Flat, verbose_name="Квартира, на которую пожаловались", null=True, on_delete=models.SET_NULL)
+    text = models.TextField("Текст жалобы")
