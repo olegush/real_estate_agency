78852985442819d48bf8fa3c84f97a077321d6a0
olegush <oleg107117@gmail.com>
2019-08-15 11:51:35 +0300

    admin features added

--- property/admin.py
+++ property/admin.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+class ComplaintInline(admin.TabularInline):
+    model = Complaint
+    extra = 0
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
+    inlines = [ComplaintInline]
+    fields = ['owner', ('owners_phonenumber', 'owners_phonenumber_pure'), 'description', ('town', 'town_district'), 'address', ('floor', 'rooms_number'), ('living_area', 'has_balcony'),  ('construction_year', 'new_building'), 'liked_by', 'price', 'active', 'created_at']
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    list_per_page = 20
 
 
 @admin.register(Complaint)
